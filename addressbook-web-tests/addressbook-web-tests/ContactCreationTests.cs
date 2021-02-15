f8439f85d2bb658c2c72a8bb90d64d6729bdea2a
AWA5CG8126DJQ\Administrator <anastasia.alekseeva@gmail.com>
2021-02-15 15:24:35 +0100

    Contact test in the same project.

---
+++ addressbook-web-tests/addressbook-web-tests/ContactCreationTests.cs
@@ -0,0 +1,170 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Text;
+
+namespace WebAddressbookTests
+{
+    [TestFixture]
+    public class ContactCreationTests
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new ChromeDriver();
+            baseURL = "http://localhost/addressbook";
+            verificationErrors = new StringBuilder();
+        }
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.AreEqual("", verificationErrors.ToString());
+        }
+
+        [Test]
+        public void ContactCreationTest()
+        {
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            InitContactCreation();
+            ContactData contact = new ContactData("Peter", "Petersson")
+            {
+                Title = "Mr",
+                Company = "Zaza",
+                Mobile = "998645",
+                Email = "ttt@hh.com",
+                Address = "Teststreet 100, 00000, Testcity"
+            };
+            FillContactForm(contact);
+            SubmitContactCreation();
+            ReturneToContactsPage();
+            Logout();
+        }
+
+        private void Logout()
+        {
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private void ReturneToContactsPage()
+        {
+            driver.FindElement(By.LinkText("home page")).Click();
+        }
+
+        private void SubmitContactCreation()
+        {
+            driver.FindElement(By.XPath("(//input[@name='submit'])[2]")).Click();
+        }
+
+        private void FillContactForm(ContactData contact)
+        {
+            FillField("firstname", contact.Firstname);
+            FillField("lastname", contact.Lastname);
+            FillField("title", contact.Title);
+            FillField("company", contact.Company);
+            FillField("address", contact.Address);
+            FillField("mobile", contact.Mobile);
+            FillField("email", contact.Email);
+            SelectFieldValue("bmonth", "1");
+            SelectFieldValue("bday", "January");
+            FillField("byear", "2000");
+        }
+
+        private void SelectFieldValue(string fieldName, string fieldValue)
+        {
+            driver.FindElement(By.Name(fieldName)).Click();
+            new SelectElement(driver.FindElement(By.Name(fieldName))).SelectByText(fieldValue);
+            driver.FindElement(By.Name(fieldName)).Click();
+        }
+
+        private void FillField(string fieldName, string fieldValue)
+        {
+            driver.FindElement(By.Name(fieldName)).Click();
+            driver.FindElement(By.Name(fieldName)).Clear();
+            driver.FindElement(By.Name(fieldName)).SendKeys(fieldValue);
+        }
+
+        private void InitContactCreation()
+        {
+            driver.FindElement(By.LinkText("add new")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
+        private bool IsElementPresent(By by)
+        {
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+        }
+
+        private bool IsAlertPresent()
+        {
+            try
+            {
+                driver.SwitchTo().Alert();
+                return true;
+            }
+            catch (NoAlertPresentException)
+            {
+                return false;
+            }
+        }
+
+        private string CloseAlertAndGetItsText()
+        {
+            try
+            {
+                IAlert alert = driver.SwitchTo().Alert();
+                string alertText = alert.Text;
+                if (acceptNextAlert)
+                {
+                    alert.Accept();
+                }
+                else
+                {
+                    alert.Dismiss();
+                }
+                return alertText;
+            }
+            finally
+            {
+                acceptNextAlert = true;
+            }
+        }
+    }
+}
+
